180dbc4ae424d1fb91241c284cde729c31e0e7ed
Алексей Глушков <a_glushkov@outlook.com>
2023-07-27 13:26:38 +0300

    Добавьте файлы проекта.

---
+++ Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureArea
+{
+    public class Circle : IFigure
+    {
+        private readonly double _radius;
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+
+        public double GetArea()
+        {
+            if (IsValid())
+                return Math.PI * _radius * _radius;
+            return 0;
+        }
+
+        public bool IsValid()
+        {
+            return _radius > 0;
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureArea
+{
+    public class Triangle : IFigure
+    {
+        private readonly double _firstSide;
+        private readonly double _secondSide;
+        private readonly double _thirdSide;
+
+        public Triangle(double firstSide, double secondSide, double thirdSide)
+        {
+            _firstSide = firstSide;
+            _secondSide = secondSide;
+            _thirdSide = thirdSide;
+        }
+
+        public double GetArea()
+        {
+            if (IsValid())
+            {
+                var value = (_firstSide + _secondSide + _thirdSide) / 2;
+                return Math.Sqrt(value * (value - _firstSide) * (value - _secondSide) * (value - _thirdSide));
+            }
+            return 0;
+        }
+
+        public bool IsValid()
+        {
+            if (_firstSide <= 0 && _secondSide <= 0 && _thirdSide <= 0)
+                return false;
+            if (_firstSide + _secondSide > _thirdSide && _firstSide + _thirdSide > _secondSide && _secondSide + _thirdSide > _firstSide)
+                return true;
+            return false;
+        }
+
+        public bool IsRectangular()
+        {
+            if (_firstSide * _firstSide == _secondSide * _secondSide + _thirdSide * _thirdSide || _secondSide * _secondSide == _firstSide * _firstSide + _thirdSide * _thirdSide || _thirdSide * _thirdSide == _firstSide * _firstSide + _secondSide * _secondSide)
+                return true;
+            return false;
+        }
+    }
+}
